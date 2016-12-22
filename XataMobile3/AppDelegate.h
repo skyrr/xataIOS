3bfd7bf3e4d93fac508f1b134c6c364c5637f800
sky <sky@mac-admin.local>
2016-12-22 17:04:28 +0300

    Initial Commit

---
+++ XataMobile3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XataMobile3
+//
+//  Created by sky on 22.12.16.
+//  Copyright © 2016 gpk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XataMobile3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XataMobile3
+//
+//  Created by sky on 22.12.16.
+//  Copyright © 2016 gpk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
